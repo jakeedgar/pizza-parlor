21655b4200729fdb9e702648ba4fe165a8ea07d5
Jake Edgar <jakeedgar1012@gmail.com>
2022-04-08 09:11:36 -0700

    WIP not yet passing third test

--- js/scripts.js
+++ js/scripts.js
@@ -3,25 +3,41 @@ function Pizza (toppings, size, price) {
   this.size = size;
   this.price = price;
 }
-let myPizza = new Pizza(['extra cheese'], "medium", 0);
+let myPizza = new Pizza(["extra cheese", "anchovies"], "medium", 0);
 console.log(myPizza);
 
-Pizza.prototype.sizePrice = function() {
-  this.price = 0;
-  if (this.size === "small") {
-    this.price += 9;
-  } else if (this.size === "medium") {
-    this.price += 10;
-  } else {
-    this.price += 12;
-  }
-  return this;
-};
-myPizza.sizePrice();
+// Pizza.prototype.sizePrice = function() {
+//   this.price = 0;
+//   if (this.size === "small") {
+//     this.price += 9;
+//   } else if (this.size === "medium") {
+//     this.price += 10;
+//   } else {
+//     this.price += 12;
+//   }
+//   return this;
+// };
+// myPizza.sizePrice();
 
-// Test: “It should return a price for each pizza size. 8 for small, 10 for medium, and 12 for large.”
-// Code: 	Let myPizza = new Pizza([], “medium””
-// 	myPizza.prototype.sizePrice();
+
+Pizza.prototype.toppingsPrice = function() {
+  let toppings = this.toppings
+  let length = toppings.length
+  console.log(length);
+  console.log(toppings);
+  this.price = 0;
+  for (i = 0; i <= toppings.length; i++)
+
+    this.toppings += this.price
+    return this;
+};
+myPizza.toppingsPrice();
+
+
+// Test: “It should return a base pizza price with no toppings.”
+// Code: 
+// 	Let myPizza = new Pizza([], “medium””
+// 	myPizza.prototype.toppingsPrice();
 // Expected Output: 10
 
 
